2f063f5cbee877e69b2cd7648f18ad8d97111134
to-test-product <to.test.product@gmail.com>
2021-08-06 23:03:22 +0900

    Fix to read fingering data

--- Jupyter/data_helper.py
+++ Jupyter/data_helper.py
@@ -10,6 +10,7 @@ def read_PIG(path):
     """
     names = ["id", "t0", "t1", "note", "_n", "_v", "ch", "_finger"]
     table = pd.read_table(path, names = names, skiprows = 1)
+    # 指替えの場合 アンダーバー繋ぎの数値が入っている
     table["fingers"] = table["_finger"].apply(lambda s: [abs(int(n)) for n in str(s).split("_")])
     table["finger"] = table["fingers"].apply(lambda s: s[0])
     table["midi"] = table["note"].apply(lambda n: mu.note.Note(n).pitch.midi)
@@ -18,12 +19,14 @@ def read_PIG(path):
 
 def read_fingering(path):
     """
-    数値列のテキストをSeriesとして読み出し
+    運指数値列のテキストをlistとして返す
+    listの要素はlistであり、音符に対応する運指（１指の場合は[1], ３＞５への指替えの場合は[3, 5]）
     czernyR = read_fingering('./data/001-Czerny_fingering_R.txt')
     """
     with open(path) as f:
         st = f.read()
-    return pd.Series([int(s) for s in re.split("", st) if s != "" and s != "_" and s != "\n"])
+    st = st.replace("_", "").replace("\n", "")
+    return [[int(n) for n in re.split("-", s)] for s in re.findall("([0-9]-[0-9]|[0-9])", st)]
 
 def convert_fingering_to_PIG(output_path, right_fingering_path, left_fingering_path, template_pig_path):
     """
@@ -37,15 +40,18 @@ def convert_fingering_to_PIG(output_path, right_fingering_path, left_fingering_p
     rights = read_fingering(right_fingering_path)
     lefts = read_fingering(left_fingering_path)
     pig = read_PIG(template_pig_path)
+    pig = pig.drop("fingers", axis = 1)
     pig = pig.drop("finger", axis = 1)
+    pig = pig.drop("midi", axis = 1)
+    pig = pig.drop("pos", axis = 1)
     r = l = 0
     for i in range(len(pig)):
         ch = pig.iloc[i]["ch"]
         if ch == 1:
-            pig.at[i, "_finger"] = "-" + str(lefts.at[l])
+            pig.at[i, "_finger"] = "_".join([str(-n) for n in lefts[l]])
             l += 1
         else:
-            pig.at[i, "_finger"] = str(rights.at[r])
+            pig.at[i, "_finger"] = "_".join([str(n) for n in rights[r]])
             r += 1
     pig.to_csv(output_path, header=False, index=False, sep = "\t")
     return pig
@@ -67,12 +73,15 @@ def fnote(name, t):
     """
     return mu.note.Note(name=name, quarterLength=flength(t))
 
-def appendPIGNotes(pig_notes, noteCallback = None):
+def appendPIGNotes(pig_notes, noteCallback):
+    appendPIGNotes(pig_notes, 0.0, noteCallback)
+
+def appendPIGNotes(pig_notes, offset, noteCallback):
     """
     PIGデータをmusic21.Noteに変換してStreamに追加
     """
     stream = mu.stream.Part()
-    time = 0.0
+    time = offset
     for i in range(len(pig_notes)):
         c = pig_notes.iloc[i]
         if(time < c.t0 and c.t0 - time > 1/32):
@@ -85,19 +94,19 @@ def appendPIGNotes(pig_notes, noteCallback = None):
         time = c.t1
     return stream
 
-def makeScore(pig, title = "", composer = "", ts = "4/4"):
+def makeScore(pig, title = "", composer = "", ts = "4/4", offset = 0.0):
     """
     PIGデータを左右手２パートの楽譜に変換
     """
     score = mu.stream.Score()
     rights = pig[pig.ch == 0]
     if len(rights) > 0:
-        pr = appendPIGNotes(rights, lambda i, n, c: n.addLyric(abs(int(str(c._finger).split("_")[0]))))
+        pr = appendPIGNotes(rights, offset, lambda i, n, c: n.addLyric(abs(int(str(c._finger).split("_")[0]))))
         pr.insert(0, mu.meter.TimeSignature(ts))
         score.insert(pr)
     lefts = pig[pig.ch == 1]
     if len(lefts) > 0:
-        pl = appendPIGNotes(lefts, lambda i, n, c: n.addLyric(abs(int(str(c._finger).split("_")[0]))))
+        pl = appendPIGNotes(lefts, offset, lambda i, n, c: n.addLyric(abs(int(str(c._finger).split("_")[0]))))
         pl.insert(0, mu.meter.TimeSignature(ts))
         score.insert(pl)
     if title is not None or composer is not None:
